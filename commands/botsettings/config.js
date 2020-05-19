b5d771e1d30afc686a2bd06a4df909be38ce8399
Glitch (sunshrine-centauri) <none>
2020-05-19 23:06:55 +0000

    🐉💈 Checkpoint ./commands/botsettings/config.js:15834686/1136

--- commands/botsettings/config.js
+++ commands/botsettings/config.js
@@ -2,10 +2,27 @@ const { MessageEmbed } = require('discord.js')
 
 module.exports = {
   name: 'config',
-  description: '',
-  category: '',
-  usage: '',
+  description: 'View the guild\'s bot config',
+  category: 'botsettings',
+  usage: '[setting]',
   run: async (client, message, args) => {
+    
+    
+    
+    let X = client.settings.get(message.guild.id)
+    if(!X || X === null || undefined) client.settings.set(message.guild.id, {
+      toggleStarboard: false,
+      toggleWelcome: true,
+      toggleAutoMod: false
+    })
+    
+    let settings = [X.toggleStarboard, X.toggleWelcome, X.toggleAutoMod]
+    settings.forEach(setting => {
+      if(X.setting === false) setting[0] = 'Not toggled on.'
+      message.channel.send(`${setting}: ${setting[0]}`)
+    })
+    
+    
     
   }
 }
