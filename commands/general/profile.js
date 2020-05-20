aed12367251e94553f669c77a5d647de2b83aad0
Glitch (sunshrine-centauri) <none>
2020-05-20 15:27:06 +0000

    🐉🚁 Checkpoint ./commands/general/profile.js:15834686/1361

--- commands/general/profile.js
+++ commands/general/profile.js
@@ -1,3 +1,25 @@
 const { MessageEmbed } = require('discord.js'),
       db = require('quick.db')
 
+module.exports = {
+  name: 'profile',
+  description: 'View your profile and stats',
+  category: 'general',
+  aliases: ['prof'],
+  run: async (client, message, args) => {
+    
+    let xp = db.fetch(`profile_${message.author.id}.xp`)
+    let premiumstatus = db.fetch(`profile_${message.author.id}.premium`)
+    let level = db.fetch(`profile_${message.author.id}.level`)
+    let acknowledgements = db.fetch(`profile_${message.author.id}.acknowlegdements`)
+    
+    if(!xp || xp === null || undefined) xp = 0
+    if(!premiumstatus || premiumstatus === null || undefined) premiumstatus = 'No premium.'
+    if(!level || level === null || undefined) level = 0
+    if(!acknowledgements || acknowledgements === null || undefined) acknowledgements = 'User'
+    
+    const embed = new MessageEmbed
+    .set
+    
+  }
+}
