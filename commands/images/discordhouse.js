4d390b054bf7e88b452d266c9359480efcc5532d
Glitch (sunshrine-centauri) <none>
2020-05-20 20:14:01 +0000

    🍰👙 Checkpoint ./commands/images/discordhouse.js:15834686/393

--- commands/images/discordhouse.js
+++ commands/images/discordhouse.js
@@ -10,9 +10,9 @@ module.exports = {
           if((!houses.map(h => h.toLowerCase()).includes(args[0].toLowerCase()))) return message.reply('please input one of the following types: \`Brilliance\`, \`Bravery\` or \`Balance\`!')
           
           const { MessageEmbed, MessageAttachment } = require('discord.js')
-          const ameClient = require("amethyste-api")
-          const ameApi = new ameClient(process.env.AMETHYSTE_KEY)
-console.log(args[0].charAt(0).toUpperCase() + args[0].slice(1))
+              const { get } = require("node-superfetch");
+          
+          const { body } = get('https://v1.api.amethyste.moe/generate/discordhouse')
           
           ameApi.generate('discordhouse', {
             'url': `${message.author.displayAvatarURL()}`,
