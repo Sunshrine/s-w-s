d2b78eaf6293d8085f1c8c315ac3cdacdc04526b
Glitch (sunshrine-centauri) <none>
2020-05-20 19:02:04 +0000

    🐄💕 Checkpoint ./commands/images/discordhouse.js:15834686/421

--- commands/images/discordhouse.js
+++ commands/images/discordhouse.js
@@ -7,15 +7,16 @@ module.exports = {
           
           let houses = ['Brilliance', 'Bravery', 'Balance']
 
-          if(args[0].toLowerCase() !== houses[0].toLowerCase() || args[0].toLowerCase() !== houses[1].toLowerCase() || args[0].toLowerCase() !== houses[2].toLowerCase()) return message.reply('please input one of the following types: \`Brilliance\`, \`Bravery\` or \`Balance\`!')
+          if((!houses.map(h => h.toLowerCase()).includes(args[0].toLowerCase()))) return message.reply('please input one of the following types: \`Brilliance\`, \`Bravery\` or \`Balance\`!')
           
           const { MessageEmbed, MessageAttachment } = require('discord.js')
           const ameClient = require("amethyste-api")
           const ameApi = new ameClient(process.env.AMETHYSTE_KEY)
+console.log(args[0].charAt(0).toUpperCase() + args[0].slice(1))
           
           ameApi.generate('discordhouse', {
             'url': `${message.author.displayAvatarURL()}`,
-            'house': args[0]
+            'house': args[0].charAt(0).toUpperCase() + args[0].slice(1)
           }).then(image => {
                     const att = new MessageAttachment(image, "discordhouse.png");
           
