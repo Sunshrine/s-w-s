8f1a0bae1e7787b11f24e8a6ff9c5bc69ee45fa2
Glitch (sunshrine-centauri) <none>
2020-05-20 18:50:18 +0000

    👨‍❤️‍👨📯 Checkpoint ./commands/images/discordhouse.js:15834686/1464

--- commands/images/discordhouse.js
+++ commands/images/discordhouse.js
@@ -4,16 +4,29 @@ module.exports = {
         category: "images",
         description: "Assigns you into Brilliance, Bravery or Balance!",
         run: async (client, message, args) => {
-
-          if(args[0])
           
-          const { MessageEmbed } = require('discord.js')
+          let houses = ['Brilliance', 'Bravery', 'Balance']
+
+          if(args[0].toLowerCase() !== houses[0].toLowerCase() || args[0].toLowerCase() !== houses[1].toLowerCase() || args[0].toLowerCase() !== houses[2].toLowerCase()) return message.reply('please input one of the following types: \`Brilliance\`, \`Bravery\` or \`Balance\`!')
+          
+          const { MessageEmbed, MessageAttachment } = require('discord.js')
+          const ameClient = require("amethyste-api")
+          const ameApi = new ameClient(process.env.AMETHYSTE_KEY)
+          
+          ameApi.generate('discordhouse', {
+            'url': `${message.author.displayAvatarURL()}`,
+            'house': args[0]
+          }).then(image => {
+                    const att = new MessageAttachment(image, "discordhouse.png");
           
           const embed = new MessageEmbed()
-          .setTitle('Here\'s your im!')
-          .setImage(body.url)
+          .setTitle('Here\'s your image!')
+          .attachFile(att)
          
           message.channel.send(embed)
+          }).catch(err => {
+                    throw err;
+          })
           
     }
 }
