16cbf34c45c4b28bb0b1edf2c0176e93a4fde3b8
Glitch (sunshrine-centauri) <none>
2020-05-28 11:47:27 +0000

    🏓💴 Checkpoint ./commands/fun/lol.js:15834686/1542

--- commands/fun/lol.js
+++ commands/fun/lol.js
@@ -1,21 +1,40 @@
 const { WebhookClient } = require('discord.js')
 
 module.exports = {
-  name: '',
-  description: '',
-  category: '',
-  usage: '',
+  name: 'lol',
+  description: 'Creates a fake message that looks like it came from a user.',
+  category: 'fun',
+  usage: '[user] <query>',
   run: async (client, message, args) => {
     
     let member = message.mentions.users.first() || message.author
-    let arg = args.slice(1).join(' ')
+    let arg;
+    
+    if(message.mentions.users.array()[0]) {
+      arg = args[1]
+    } else arg = args[0]
     
     if(!message.member.hasPermission(['ADMINISTRATOR'])) {
       message.delete()
       return message.reply('you don\'t have permission to use the \`\`lol\`\` command! \nMissing Permission: \`\`\ADMINSTRATOR`\`')
     }
     
+    if(!arg) {
+      message.delete()
+      return message.reply('please enter something!')
+    }
+    
     message.delete()
+    
+    const webhooks = await message.channel.fetchWebhooks();
+const myWebhooks = webhooks.filter(webhook => webhook.owner.id === client.user.id && webhook.name === member.username);
+
+  if (myWebhooks.size === 0) return;
+  for (let [id, webhook] of myWebhooks) await webhook.delete(`Requested by ${message.author.tag}`);
+
+
+
+
     let webhook = await message.channel.createWebhook(member.username, {
       avatar: member.displayAvatarURL({ dynamic: true })
     })
