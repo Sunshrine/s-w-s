e7c6ba71e06fbd4766ab861c1711a2aa066067b9
Glitch (sunshrine-centauri) <none>
2020-05-30 09:58:17 +0000

    🍗🏙 Checkpoint ./commands/fun/lol.js:15834686/1539

--- commands/fun/lol.js
+++ commands/fun/lol.js
@@ -10,8 +10,8 @@ module.exports = {
     let arg;
 
     if (message.mentions.users.array()[0]) {
-      arg = args[1];
-    } else arg = args[0];
+      arg = args.slice(1).join(' ')
+    } else arg = args.slice(0).join(' ')
 
     if (!message.member.hasPermission(["ADMINISTRATOR"])) {
       message.delete();
@@ -32,33 +32,13 @@ module.exports = {
       webhook =>
         webhook.owner.id === client.user.id && webhook.name === member.username
     );
-
-    if (myWebhooks.size === 0) {
-      let webhookr = await message.channel.createWebhook(member.username, {
-              avatar: member.displayAvatarURL({ dynamic: true })
-            });
-
-            const webhookClient = new WebhookClient(webhookr.id, webhookr.token);
-            webhookClient.send(arg);
+    
+    if(myWebhooks || myWebhooks.size !== 0 || myWebhooks !== null ? null : undefined) {
+      let instance1 = new WebhookClient(myWebhooks.id, myWebhooks.token)
+      instance1.send(arg)
+    } else {
+      let instance2 = message.channel.creart
     }
-    for (let [id, webhook] of myWebhooks)
-      await webhook
-        .delete(`Requested by ${message.author.tag}`)
-        .then(async () => {
-          const webhooks2 = await message.channel.fetchWebhooks();
-          const myWebhooks2 = webhooks.filter(
-            webhook =>
-              webhook.owner.id === client.user.id &&
-              webhook.name === member.username
-          );
-          if (myWebhooks2.size === 0 || !myWebhooks2) {
-            let webhooke = await message.channel.createWebhook(member.username, {
-              avatar: member.displayAvatarURL({ dynamic: true })
-            });
 
-            const webhookClient = new WebhookClient(webhooke.id, webhooke.token);
-            webhookClient.send(arg);
-          }
-        });
-  }
+  },
 };
