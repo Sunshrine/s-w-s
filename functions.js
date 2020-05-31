e79f11934af863198fc080b0f1deab35ccc9402d
Glitch (sunshrine-centauri) <none>
2020-05-31 09:43:07 +0000

    🌆🎓 Checkpoint ./functions.js:15834686/1240

--- functions.js
+++ functions.js
@@ -58,39 +58,5 @@ module.exports = {
     return client.users.cache.get(id);
     },
   
-  generateCode: function () {
-    let code = '';
-    let dict = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    for(var i = 0; i < 19; i++){
-        code = code + dict.charAt(Math.floor(Math.random() * dict.length));
-    }
-    return code;
-},
-  
-  toCheckNitro: function (code) {
-    let working = []
-   request(`https://discordapp.com/api/v6/entitlements/gift-codes/${code}?with_application=false&with_subscription_plan=true`, (error, res, body) => {
-        if(error){
-            console.error(`Error: ${error}`);
-            return;
-        }
-        try {
-            body = JSON.parse(body);
-            if(body.message != "You are being rate limited."){
-                console.warn(`Working Code: https://discord.gift/${code}`);
-                console.log(JSON.stringify(body, null, 4));
-                working.push(`https://discord.gift/${code}`);
-                fs.writeFileSync(__dirname + '/codes.json', JSON.stringify(working, null, 4));
-            }
-            else {
-                console.warn(`${code} invalid`);
-            }
-        }
-        catch (error) {
-            console.error(`Error:`);
-            console.error(error);
-            return;
-        }
-    });
-}
+
   }
