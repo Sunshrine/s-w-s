de1c8215d4c3e960086233acd353755c78337c9f
Glitch (sunshrine-centauri) <none>
2020-06-05 18:19:19 +0000

    💷👧 Checkpoint ./commands/images/gay.js:15834686/3238

--- commands/images/gay.js
+++ commands/images/gay.js
@@ -1,6 +1,4 @@
-const Discord = require("discord.js"),
-  { post } = require("node-superfetch");
-const token = process.env.AMETHYSTE_KEY;
+const Discord = require("discord.js")
 
 module.exports = {
   name: "gay",
@@ -9,8 +7,7 @@ module.exports = {
   usage: "[mention] || [attachment]",
   run: async (client, message, args) => {
     let attachment =
-      message.mentions.users.first() ||
-      message.author.displayAvatarURL();
+      message.mentions.users.first() || message.author.displayAvatarURL();
     if (message.mentions.users.array()[0]) {
       let user = message.mentions.users.array[0];
       attachment = user.displayAvatarURL();
@@ -19,13 +16,21 @@ module.exports = {
     let msg = await message.channel.send("Generating...");
 
     try {
-      let { body } = await post("https://v1.api.amethyste.moe/generate/gay")
-        .query({ url: attachment })
-        .set("Authorization", `Bearer ${token}`);
-      let s = new Discord.MessageAttachment(body, "tobecontinued.png");
-      message.delete();
-      msg.delete({ timeout: 5000 });
-      message.channel.send(s);
+      const ameClient = require("amethyste-api");
+      const ameApi = new ameClient(process.env.AMETHYSTE_KEY);
+      ameApi
+        .generate("gay", {
+          url: attachment
+        })
+        .then(image => {
+          let s = new Discord.MessageAttachment(image, "tobecontinued.png");
+          message.delete();
+          msg.delete({ timeout: 5000 });
+          message.channel.send(s);
+        })
+        .catch(err => {
+          throw err;
+        });
     } catch (e) {
       console.log(e);
       msg.edit(`An error happened! Please try again later!`);
