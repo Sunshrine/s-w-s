5d64d495d53d69583024048a918197f8ae6f42af
Glitch (sunshrine-centauri) <none>
2020-06-06 11:03:55 +0000

    ⛹🛤 Checkpoint ./commands/economy/balance.js:15834686/375

---
+++ commands/economy/balance.js
@@ -0,0 +1,13 @@
+const { MessageEmbed } = require('discord.js')
+const db = require('quick.db')
+
+module.exports = {
+  name: 'balance',
+  description: 'Check your coins balance.',
+  aliases: ['bal', 'coinbal'],
+  category: 'economy',
+  run: async (client, message, args) => {
+    
+    client.emojis
+  }
+}
