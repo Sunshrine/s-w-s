7ed86f43d0e48cc8f2cda509d2a2b968984f7c8c
Glitch (sunshrine-centauri) <none>
2020-06-06 11:26:54 +0000

    👩‍👩‍👦🐜 Checkpoint ./commands/economy/daily.js:15834686/268

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -1,5 +1,6 @@
 const { MessageEmbed } = require('discord.js')
-const db = require('quick.db')
+const db = require('quick.db'),
+      ms = require('parse-ms')
 
 module.exports = {
   name: 'daily',
@@ -9,7 +10,13 @@ module.exports = {
   run: async (client, message, args) => {
     
     let cooldown = 8.64e+7,
-        amount = Math.floor((Math))
+        amount = Math.floor((Math.random() * 250) + 1)
+    
+    let lastdaily = await db.fetch(`lastDaily_${message.author.id}`)
+    
+    if(lastdaily !== null && cooldown - (Date.now() - lastdaily) > 0) {
+      
+    }
     
     
   }
