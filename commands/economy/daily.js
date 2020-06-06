1b3d7cd844b1a84a0d9817b45e5f535602f39ef9
Glitch (sunshrine-centauri) <none>
2020-06-06 11:38:54 +0000

    ⚾️🌝 Checkpoint ./commands/economy/daily.js:15834686/1387

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -1,6 +1,7 @@
 const { MessageEmbed } = require('discord.js')
 const db = require('quick.db'),
-      ms = require('parse-ms')
+      ms = require('parse-ms'),
+      dailystatus = new MessageEmbed()
 
 module.exports = {
   name: 'daily',
@@ -11,12 +12,27 @@ module.exports = {
     
     let cooldown = 8.64e+7,
         amount = Math.floor((Math.random() * 250) + 1)
+
     
     let lastdaily = await db.fetch(`lastDaily_${message.author.id}`)
     
     if(lastdaily !== null && cooldown - (Date.now() - lastdaily) > 0) {
+      let timeobj = ms(cooldown - (Date.now() - lastdaily))
       
+      dailystatus.setColor('RED')
+      dailystatus.setTitle('Reward already collected!')
+      dailystatus.setDescription(`${message.member}, please wait *${timeobj.hours}* **hours**, *${timeobj.minutes}* **minutes** and *${timeobj.seconds}* **seconds**!`)
+    } else {
+      
+      let coin = client.emojis.cache.get(718780405481734175)
+      dailystatus.setColor('GREEN')
+      dailystatus.setTitle('Reward collected!')
+      dailystatus.setDescription(`${message.member}, successfully collected ${coin} ${amount}!`)
+      
+      db.add(`coinBalance`)
     }
+    
+    
     
     
   }
