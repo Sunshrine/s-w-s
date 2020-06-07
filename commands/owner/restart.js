e689b7bb06dcc0778e5e6dce9322332d0099c8be
Glitch (sunshrine-centauri) <none>
2020-06-07 13:26:15 +0000

    🐋🍔 Checkpoint ./commands/owner/restart.js:15834686/467

--- commands/owner/restart.js
+++ commands/owner/restart.js
@@ -10,11 +10,14 @@ module.exports = {
     if(message.author.id != "685371966022352928") return message.channel.send("You're not the bot the owner!").then(m => m.delete({ "timeout": 1500 }))
 
     try {
-        let sx = await message.channel.send("<a:loading:719121979319255052> Attempting a restart...")
-        .then(client.destroy())
-        .then(() => client.login(token).then(() => {
-            sx.edit('<:success:719123112964128830> Successfully restarted!')
-          }))
+        let sx = await message.channel.send("<a:loading:719121979319255052> Attempting a restart...").then (() => {
+       setTimeout(() => { 
+         message.channel.send('Restarted.')
+       }, 10000)
+        })
+         .then(() => {
+       process.exit(1);
+})
         
     } catch(e) {
         message.channel.send(`ERROR: ${e.message}`)
