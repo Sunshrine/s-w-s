cec19e9d1538650415ba477a82ca53f9fddbbdcc
Glitch (sunshrine-centauri) <none>
2020-06-07 09:42:13 +0000

    💄🙆 Checkpoint ./commands/owner/restart.js:15834686/857 ./commands/owner/shutdown.js:15834686/9

--- commands/owner/restart.js
+++ commands/owner/restart.js
@@ -3,16 +3,26 @@ module.exports = {
         description: "Shuts down the bot!",
         category: "owner",
         aliases: ["botstop", "off", "stop", "end"],
-        run: async (bot, message, args) => {
+        run: async (client, message, args) => {
 
     if(message.author.id != "685371966022352928") return message.channel.send("You're not the bot the owner!").then(m => m.delete({ "timeout": 1500 }))
 
     try {
-        await message.channel.send("Bot is shutting down...")
-        process.exit()
-    } catch(e) {
-        message.channel.send(`ERROR: ${e.message}`).then(m => m.delete({ "timeout": 1500 }))
-    }
+        message.channel.send("<a:loading:719121979319255052> Attempting a restart...").then(msg => {
+          msg.react('🆗');
+          setTimeout(function() {
+             msg.edit("<:success:719123112964128830> I should be back up now!");
+          }, 10000);
+        })
+        .then(client.destroy())
+        .then(client.login(process.env.TOKEN))
+
+
+
+
+          } catch(e) {
+            message.channel.send(`ERROR: ${e.message}`)
+          }
     
 
 
--- commands/owner/shutdown.js
+++ commands/owner/shutdown.js
@@ -3,7 +3,7 @@ module.exports = {
         description: "Shuts down the bot!",
         category: "owner",
         aliases: ["botstop", "off", "stop", "end"],
-        run: async (bot, message, args) => {
+        run: async (client, message, args) => {
 
     if(message.author.id != "685371966022352928") return message.channel.send("You're not the bot the owner!").then(m => m.delete({ "timeout": 1500 }))
 
