17c5f3d61f2fa76109cb508e51cf47ddc597686e
Glitch (sunshrine-centauri) <none>
2020-06-07 11:30:13 +0000

    🍀🏠 Checkpoint ./commands/images/triggered.js:15834686/672

--- commands/images/triggered.js
+++ commands/images/triggered.js
@@ -1,5 +1,5 @@
 const token = 'Njg1MzcxOTY2MDIyMzUyOTI4.SoTVDCamJwb0GZ1gpipyItkkg2bWtWs8ZHiaWQbP1gc'
-const premium = require('../../premiumusers.json')
+const db = require('quick.db')
 
 module.exports = {
     name: "triggered",
@@ -11,7 +11,18 @@ module.exports = {
 
         message.delete()
 
-        if(!premium.users.includes(message.author.id)) return message.reply('you don\'t have premium!').then(m => m.delete({ "timeout": 1500 }))
+        let userdata = db.fetch(`userData_${message.author.id}`)
+    if(!userdata || userdata === null || userdata === undefined) {
+      db.set(`userData_${message.author.id}`, { indexed: {
+        "premium": 'none',
+        acknowledgements: 'none'
+      }, global: {
+        "username": message.author.username,
+        "avatarlink": message.author.avatarURL({ dynamic: true }),
+        "id": message.author.id,
+        "tag": message.author.tag
+      }})
+    }
 
     let target = message.mentions.users.first() || message.guild.members.cache.get(args[0]) || message.author;
     if(target === message.guild.members.cache.get(args[0])) {
