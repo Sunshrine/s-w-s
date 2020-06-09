c8265302e6b04cb5a72d34eedd61dc783b993f3c
Glitch (sunshrine-centauri) <none>
2020-06-09 13:58:40 +0000

    🛢🐷 Checkpoint ./commands/general/recipe.js:15834686/3117

---
+++ commands/general/recipe.js
@@ -0,0 +1,12 @@
+const { MessageEmbed } = require('discord.js');
+const request = require('node-superfetch');
+
+module.exports = {
+			name: 'recipe',
+			aliases: ['recipe-puppy'],
+			category: 'search',
+			description: 'Searches for recipes based on your query.',
+      run: async (client, message, args) => {
+      }
+  
+      
