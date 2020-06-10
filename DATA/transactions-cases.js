26b8610cc4fec977bfed298df8d98d2827110a25
Glitch (sunshrine-centauri) <none>
2020-06-10 12:27:18 +0000

    👓💸 Checkpoint ./DATA/transactions-cases.js:15834686/369 ./commands/economy/transactions.js:15834686/518

--- DATA/transactions-cases.js
+++
@@ -1,3 +0,0 @@
-module.exports = (client, message) => {
-  case 1: 
-}
---
+++ commands/economy/transactions.js
@@ -0,0 +1,17 @@
+const db = require('quick.db')
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+  name: 'transactions',
+  description: 'Do certain transactions.',
+  usage: 'to open up the menu',
+  aliases: ['actionsmenu', 'transactionsmenu', 'tmenu'],
+  category: 'economy',
+  run: async (client, message, args) => {
+    
+    int (num = 0);
+    switch (num) {
+        
+    }
+  }
+}
