6af6de3798880f9cf12fd848f9cbea8b338f7110
Glitch (sunshrine-centauri) <none>
2020-06-10 20:59:43 +0000

    🏁👨‍👨‍👦 Checkpoint ./commands/economy/give.js:15834686/5452

--- commands/economy/give.js
+++ commands/economy/give.js
@@ -1,46 +1,69 @@
 const { MessageEmbed } = require("discord.js");
-const db = require("quick.db")
+const db = require("quick.db");
 
 module.exports = {
-  name: 'give',
-  description: 'Give a user an item from your inventory.',
-  usage: '<user> <item>',
-  category: 'economy',
-  aliases: ['giveitem'],
+  name: "give",
+  description: "Give a user an item from your inventory.",
+  usage: "<user> <item>",
+  category: "economy",
+  aliases: ["giveitem"],
   run: async (client, message, args) => {
-    
     const nouserembed = new MessageEmbed()
-    .setColor('RED')
-    .setDescription(`<:no:720295035085783103> Please mention a user!`)
-    
-    const noitemembed = new MessageEmbed()
-    .setColor('RED')
-    .setDescription(`<:no:720295035085783103> Please input an item!`)
-    
-    const itemdoesnotexist = new MessageEmbed()
-    .setColor('RED')
-    .setDescription(`<:no:720295035085783103> That item does not exist; either you don't have it or it isn't a valid item!`)
-    
-    if(!message.mentions.members.first()) message.reply(nouserembed)
-    if(!args[1]) message.reply(noitemembed)
-    
-          let inventory = db.get(`userData_${message.author.id}.inventory`);
-      let lowerCaseInventory = inventory.map(v => v.toLowerCase())
-      if (!lowerCaseInventory.includes(args[0].toLowerCase()))
-        return message.reply(
+      .setColor("RED")
+      .setDescription(`<:no:720295035085783103> Please mention a user!`);
 
-      Array.prototype.remove = function() {
-        var what,
-          a = arguments,
-          L = a.length,
-          ax;
-        while (L && this.length) {
-          what = a[--L];
-          while ((ax = this.indexOf(what)) !== -1) {
-            this.splice(ax, 1);
-          }
+    const noitemembed = new MessageEmbed()
+      .setColor("RED")
+      .setDescription(`<:no:720295035085783103> Please input an item!`);
+
+    const itemdoesnotexist = new MessageEmbed()
+      .setColor("RED")
+      .setDescription(
+        `<:no:720295035085783103> That item does not exist; either you don't have it or it isn't a valid item!`
+      );
+    
+    if (!message.mentions.members.first()) return message.reply(nouserembed);
+    if (!args[1]) return message.reply(noitemembed);
+
+    let user = message.mentions.members.first();
+
+    let inventory = db.get(`userData_${message.author.id}.inventory`);
+    if (!inventory || !inventory.length) inventory = "No items bought.";
+    let lowerCaseInventory = inventory.map(v => v.toLowerCase());
+    if (!lowerCaseInventory.includes(args[0].toLowerCase()))
+      return message.reply(itemdoesnotexist);
+
+    Array.prototype.remove = function() {
+      var what,
+        a = arguments,
+        L = a.length,
+        ax;
+      while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+          this.splice(ax, 1);
         }
-        return this;
-      };
+      }
+      return this;
+    };
+    
+        const gifteditem = new MessageEmbed()
+      .setColor("GREEN")
+      .setDescription(
+        `<:yes:720295144296939651> You have sent item \`\`${args[1].replace(
+          /^./,
+          v => v.toUpperCase()
+        )}\`\` to ${user}!`
+      );
+
+
+    let x = await inventory.remove(args[0]);
+    db.push(
+      `userData_${user.user.id}.items`,
+      args[1].replace(/^./, v => v.toUpperCase())
+    );
+    db.set(`userData_${message.author.id}.inventory`, x)
+    
+    await message.reply(gifteditem)
   }
-}
+};
