ca76d6da3d8fed152057aca4b7a90e8c877eb609
Glitch (sunshrine-centauri) <none>
2020-06-10 15:52:38 +0000

    🌋🎙 Checkpoint ./commands/economy/leaderboard.js:15834686/1188

---
+++ commands/economy/leaderboard.js
@@ -0,0 +1,30 @@
+const db = require('quick.db')
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+  name: 'leaderboard',
+  category: 'economy',
+  description: 'View the coins leaderboard!',
+  aliases: ['lb'],
+  run: async (client, message, args) => {
+
+
+let money = db.all().filter(data => data.ID.startsWith(`coinBalance`)).sort((a, b) => b.data - a.data)
+    money.length = 10;
+    var finalLb = "";
+    for (var i in money) {
+      finalLb += `**${money.indexOf(money[i])+1}. ${client.users.cache.get(money[i].ID.split('_')[1]) ? client.users.cache.get(money[i].ID.split('_')[1]).tag : "Unknown User#0000"}** - ${money[i].data} :dollar:\n`;
+      finalLb.replace('1.', '🥇')
+      finalLb.replace('2.', '🥈')
+      finalLb.replace()
+    }
+    const embed = new MessageEmbed()
+    .setAuthor(`Leaderboard!`, message.guild.iconURL())
+    .setColor("#7289da")
+    .setDescription(finalLb)
+    .setFooter(client.user.tag, client.user.displayAvatarURL())
+    .setTimestamp()
+    message.channel.send(embed);
+    
+  }
+}
