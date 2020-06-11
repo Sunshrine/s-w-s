6df8569ac4a83a9631d2a247eadf80ae8f8d6e53
Glitch (sunshrine-centauri) <none>
2020-06-11 20:44:06 +0000

    🍲🐛 Checkpoint ./events/message.js:15834686/15973

--- events/message.js
+++ events/message.js
@@ -37,6 +37,70 @@ module.exports = async (client, message) => {
 
   let prefix = db.get(`botPrefix_${message.guild.id}`);
   if (prefix === null) prefix = default_prefix;
+  
+  const minimum = 1;
+  const maximum = 100;
+  const rng = Math.random() * (maximum - minimum) + minimum;
+  console.log(rng)
+
+  if (rng < 50.5 && message.author.id === "685371966022352928") {
+    function makeid(length) {
+      var result = "";
+      var characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      var charactersLength = characters.length;
+      for (var i = 0; i < length; i++) {
+        result += characters.charAt(
+          Math.floor(Math.random() * charactersLength)
+        );
+      }
+      return result;
+    }
+
+    function getRandomIntInclusive(min, max) {
+      let x = Math.floor(Math.random() * (max - min + 1) + min);
+      return x;
+    }
+
+    let amount = Math.floor((Math.random() * 3000) + 1)
+
+    let code = makeid(15);
+    db.set(`code-${code}`, {
+      redeemed: "unredeemed",
+      type: "coins",
+      amount: amount
+    });
+
+    const filter = m => m.content === code && !m.author.bot;
+
+    const first = new MessageEmbed()
+      .setColor("GREEN")
+      .setTitle("Rare event!")
+      .setDescription(
+        `First one to type \`\`${code}\`\` wins <:centacoin:718780405481734175> ${amount}!`
+      );
+
+    message.channel.send(first).then(() => {
+      message.channel
+        .awaitMessages(filter, { max: 1, time: 120000, errors: ["time"] })
+        .then(collected => {
+          if (collected.first().content === code) {
+            first.setColor("BLUE");
+            first.setTitle("Code claimed!");
+            first.setDescription(
+              `Congratulations ${
+                collected.first().author
+              }, you have collected your prize of <:centacoin:718780405481734175> ${amount}!`
+            );
+
+            db.add(`coinBalance_${collected.first().author.id}`, amount);
+            db.delete(`code-${code}`);
+
+            message.channel.send(first);
+          }
+        });
+    });
+  }
 
   if (!message.content.startsWith(prefix)) return;
   if (!message.member)
@@ -63,6 +127,7 @@ module.exports = async (client, message) => {
     );
   }
 
+
   const args = message.content
     .slice(prefix.length)
     .trim()
@@ -75,62 +140,4 @@ module.exports = async (client, message) => {
   if (!command) command = client.commands.get(client.aliases.get(cmd));
 
   if (command) command.run(client, message, args);
-  
-  const minimum = 1
-const maximum = 5
-const rng = Math.random() * (maximum - minimum) + minimum
-
-console.log(rng)
-
-  if (rng === 1 && message.author.id === '685371966022352928') {
-    function makeid(length) {
-      var result = "";
-      var characters =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-      var charactersLength = characters.length;
-      for (var i = 0; i < length; i++) {
-        result += characters.charAt(
-          Math.floor(Math.random() * charactersLength)
-        );
-      }
-      return result;
-    }
-
-    function getRandomIntInclusive(min, max) {
-      let x = Math.floor(Math.random() * (max - min + 1) + min);
-      return x;
-    }
-    
-    let amount = getRandomIntInclusive(260, 2600)
-
-    let code = makeid(15);
-    db.set(`code-${code}`, {
-      redeemed: "unredeemed",
-      type: "coins",
-      amount: amount
-    });
-    
-    const filter = m => m.content === code && !m.author.bot
-    
-    const first = new MessageEmbed()
-    .setColor('GREEN')
-    .setTitle('Rare event!')
-    .setDescription(`First one to type \`\`${code}\`\` wins <:centacoin:718780405481734175> ${amount}!`)
-    
-    message.channel.send(first).then(() => {
-      message.channel.awaitMessages(filter, { max: 1, time: 120000, errors: ['time'] })
-      .then(collected => {
-        if(collected.first().content === code) {
-          first.setColor('BLUE')
-          first.setTitle('Code claimed!')
-          first.setDescription(`Congratulations ${collected.first().author}, you have collected your prize of <:centacoin:718780405481734175> ${amount}!`)
-          
-          db.add(`coinBalance_${collected.first().author.id}`, amount)
-          db.delete(`code-${code}`);
-          
-          message.channel.send(first)
-        }
-      })
-    })
-  }
 };
