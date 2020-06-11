98281ec20bb23a26d72c62cfa51493f6db48f6f1
Glitch (sunshrine-centauri) <none>
2020-06-11 13:22:05 +0000

    🗃🐃 Checkpoint ./events/messageDelete.js:15834686/411

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -5,15 +5,17 @@ module.exports = async (client, message) => {
       image: message.attachments.first() ? message.attachments.first().proxyURL : null
     })
   
+  const image = message.attachments.first() ? message.attachments.first().proxyURL : null
   const { WebhookClient, MessageEmbed } = require('discord.js')
   
-    let webhook = new WebhookClient('719929466931118110', 'Gu6_T49edPjSoSdB2w6e7g4K7rer_EC6FGvXCGtptZWKY6GYCEnTmIl79Rb1pZP0-iYt')
+    let webhook = new WebhookClient('720627238659293184', 'XooM_3qKk3uYsAoMnKhd9DJhfAHlQzwfcmiRDa40El0e2dLs7MA233MQLZ1hbHMDNMV8')
     const success = new MessageEmbed()
     .setColor('BLUE')
     .setTitle(`A message by ${message.author.tag} in ${message.guild.name} was deleted.`)
+    .setThumbnail(message.author.dispalyAvatarURL())
     
     if(message.content) success.setDescription(message.content)
-    if(message.atttachments.first() !== null) success.setImage(message.attachments.first().proxyURL)
+    if(image && image !== null) success.setImage(message.attachments.first().proxyURL)
   
   webhook.send(success)
 }
