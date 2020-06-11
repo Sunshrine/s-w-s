a6dfe4e58795159748941fa0ee26ce826ef3d798
Glitch (sunshrine-centauri) <none>
2020-06-11 14:39:49 +0000

    🍗🎖 Checkpoint ./commands/economy/voterewards.js:15834686/1442

--- commands/economy/voterewards.js
+++ commands/economy/voterewards.js
@@ -1,4 +1,5 @@
-const db = require('quick.db')
+const db = require('quick.db'),
+      ms = require('parse-ms')
 const { MessageEmbed, WebhookClient } = require('discord.js')
 
 module.exports = {
@@ -14,7 +15,11 @@ let x = Math.floor(Math.random() * ((max-min)+1) + min);
 }
     
     function generateEmbed(name, color, title, description) {
-      const name 
+      name = new MessageEmbed()
+      .setColor(color)
+      .setTitle(title)
+      .setDescription(description)
+      return message.reply(name)
     }
     
         let cooldown = 4.32e+7,
@@ -26,7 +31,33 @@ let x = Math.floor(Math.random() * ((max-min)+1) + min);
         .set('Authorization', process.env.DBL_TOKEN)
         .query({ userId: message.author.id })
         
-        if(body.vote === 0)
+        if(body.vote === 0) {
+          const novote = new MessageEmbed() 
+          .setColor('RED') 
+          .setDescription('<:no:720295035085783103> You did not vote, please vote [here](https://top.gg/bot/692374798654898260/vote)!')
+          
+          message.reply(novote)
+          }
+    
+        let lastvote = await db.fetch(`lastVote_${message.author.id}`)
+    
+    if(lastvote !== null && cooldown - (Date.now() - lastvote) > 0 && body.vote === 1) {
+      let timeobj = ms(cooldown - (Date.now() - lastvote))
+      
+      
+      const votestatus = new MessageEmbed()
+      .setColor('RED')
+      .setTitle('Reward already collected!')
+      .setDescription(`${message.member}, please wait *${timeobj.hours}* **hours**, *${timeobj.minutes}* **minutes** and *${timeobj.seconds}* **seconds**!`)
+      
+      message.reply(votestatus)
+    } else {
+      if(vote !== 1) return;
+      
+      c
+    }
+      
+    
         
   }
 }
