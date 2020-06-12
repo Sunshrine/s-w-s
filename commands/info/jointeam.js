124db706e81e4a29bf56bb37209484c778d0d442
Glitch (sunshrine-centauri) <none>
2020-06-12 13:41:45 +0000

    🚪🌕 Checkpoint ./commands/info/jointeam.js:15834686/300

---
+++ commands/info/jointeam.js
@@ -0,0 +1,11 @@
+const { MessageEmbed, WebhookClient } = require('discord.js')
+
+module.exports = {
+  name: 'jointeam',
+  description: 'Join Centauri\'s development team!',
+  category: 'info',
+  aliases: ['join'],
+  run: async (client, message, args) => {
+    
+  }
+}
