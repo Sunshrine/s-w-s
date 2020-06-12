ef672456e2f4053f32f327a59129f4004877023b
Glitch (sunshrine-centauri) <none>
2020-06-12 13:53:46 +0000

    🍔🎅 Checkpoint ./commands/info/jointeam.js:15834686/507

--- commands/info/jointeam.js
+++ commands/info/jointeam.js
@@ -7,5 +7,17 @@ module.exports = {
   aliases: ['join'],
   run: async (client, message, args) => {
     
+    message.channel.createWebhook('Centauri Dev', {
+      avatar: 'https://cdn.discordapp.com/attachments/720613045038219365/720998354741493800/512x512bb-dev.jpg'
+    }.then(webhook => {
+      const join = new MessageEmbed()
+      .setColor('GREEN')
+      .setDescription('Please wait until CentaDev applications are made!')
+      
+      webhook.send(join).then(async () => {
+        await webhook.delete()
+      })
+    }))
+    
   }
 }
