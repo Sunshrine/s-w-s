ecc1085ec849f7fd31d605b67cdef11bb9bf9f4d
Glitch (sunshrine-centauri) <none>
2020-06-14 11:01:52 +0000

    🐡📕 Checkpoint ./commands/mod/createlottery.js:15834686/5206 ./functions2.js:15834686/1014 ./events/message.js:15834686/206

--- commands/mod/createlottery.js
+++ commands/mod/createlottery.js
@@ -1,25 +1,56 @@
-const { createLottery } = require('../../functions2.js'),
-      ms = require('ms'),
-      ms2 = require('parse-ms'),
-      db = require('quick.db')
+const ms = require("ms"),
+  { MessageEmbed } = require("discord.js"),
+  db = require("quick.db");
 
 module.exports = {
-  name: 'createlottery',
-  description: 'Create a coins lottery!',
-  category: 'mod',
-  aliases: ['createl'],
-  usage: '<m/d/w (for example 1m [1 minute], 3d [3 days] and 9w [9 weeks])>',
+  name: "createlottery",
+  description: "Create a coins lottery!",
+  category: "mod",
+  aliases: ["createl"],
+  usage: "<m/d/w (for example 1m [1 minute], 3d [3 days] and 9w [9 weeks])>",
   run: async (client, message, args) => {
-    
-    if(!args[0].includes('s') || !args[0].includes('m') || !args[0].includes('d') || args[0].includes('w')) {
-      return message.reply('please insert how long the lottery lasts, for example: 1w (1 week)')
+    async function createLottery(time, channelID) {
+      if (isNaN(time) || time < 0) {
+        return null;
+      }
+
+      if (isNaN(channelID) || channelID < 0) {
+        return null;
+      }
+
+      let channel = client.channels.cache.get(channelID);
+      if (!channel) return;
+
+      var startAt = Date.now();
+      var endAt = Date.now() + time;
+
+      var duration = endAt - Date.now();
+
+      var embed = new MessageEmbed()
+        .setColor("BLUE")
+        .setTitle("🎉 Lottery 🎉")
+        .setDescription(
+          `__Time Remaining:__ ${ms(duration, { long: true })}`
+        )
+        .setFooter(`To join contribute using the lottery command.`)
+        .setTimestamp(`Ends At | ${new Date(endAt).toISOString()}`);
+
+      let x = await channel.send(embed);
+
+      setInterval(() => {
+        embed.setDescription(
+          `__Time Remaining:__ ${ms(duration, { long: true })}`
+        );
+        x.edit(embed);
+      }, 10000);
     }
-    
-    let lasts = ms(args[0])
-    
-    createLottery(lasts, message.channel.id).then(id => {
-      db.set(`lottery_${message.guild.id}`, { prize: 0 })
-    })
-    
+
+    if (!args[0]) return message.reply("please enter a time!");
+
+    let lasts = ms(args[0]);
+
+    createLottery(lasts, message.channel.id)
+      await db.set(`lottery_${message.guild.id}`, { prize: 0 });
+
   }
-}
+};
--- functions2.js
+++ functions2.js
@@ -1,37 +1,4 @@
 module.exports = (client) => {
-  const { prefix } = require('/events/message.js')
-  const ms = require('parse-ms')
-  const { MessageEmbed } = require('discord.js')
-  async function createLottery(time, channelID) {
-    if(isNaN(time) || time < 0) {
-      return null
-    }
-    
-    if(isNaN(channelID) || channelID < 0) {
-      return null
-    }
-    
-    var startAt = Date.now()
-    var endAt = Date.now() + time
-    
-    var duration = endAt - Date.now();
-    
-    var embed = new MessageEmbed()
-    .setColor('BLUE')
-    .setTitle('🎉 Lottery 🎉')
-    .setDescription(`__Time Remaining:__ ${ms(ms(duration), { long: true })}`)
-    .setFooter(`To join contribute using ${prefix}lottery <amount>.`)
-    .setTimestamp(`Ends At | ${new Date(endAt).toISOString()}`)
-    
-    let channel = client.channels.cache.get(channelID)
-    if(!channel) return
-    
-    let x = await channel.send(embed)
-    
-    
-    setInterval(() => {
-      embed.setDescription(`__Time Remaining:__ ${ms(ms(duration), { long: true })}`)
-      x.edit(embed)
-    }, 60000)
-  }
+
+ 
 }
